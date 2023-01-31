828227269c31cce24ef2b0b5336846fd6f551b2a
Ayesha Khan <ayeshakay786@gmail.com>
2023-01-30 22:13:32 -0800

    Initial commit

---
+++ Assets/Scripts/HelloWorlds.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorlds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int m_frame = 0;
+    void Start()
+    {
+        Debug.Log("Hello Worlds");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
